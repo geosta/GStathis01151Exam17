4cf5b0e17bd267497d3c091ae77b36d7f2be3c74
geosta <it01151@uom.edu.gr>
2017-01-27 15:22:31 +0200

    1st

---
+++ app/src/main/java/gr/stathis/george/gstathis01151/MerchantAdapter.java
@@ -0,0 +1,57 @@
+package gr.stathis.george.gstathis01151;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by user on 27/1/2017.
+ */
+
+public class MerchantAdapter extends ArrayAdapter<Merchant> {
+
+    private ArrayList<Merchant> merchants;
+    private Context context;
+
+    public MerchantAdapter(Context context, ArrayList<Merchant> objects) {
+        super(context, 0, objects);
+        this.merchants = objects;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+        ViewHolder viewHolder;
+        Merchant m = merchants.get(position);
+
+        if(rowView == null){
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.list_item_merchant, null);
+            viewHolder = new ViewHolder(rowView);
+            rowView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder)rowView.getTag();
+        }
+
+        viewHolder.imageView.setImageResource(R.drawable.yummy_logo);
+        viewHolder.legalNameView.setText(m.getLegalName());
+        //.....
+        return  rowView;
+    }
+
+    static class ViewHolder {
+        public final ImageView imageView;
+        public final TextView legalNameView;
+        public final TextView categoryNameView;
+        public final TextView addressView;
+        public final TextView ratingView;
+
+        public ViewHolder(View view){
+            imageView = (ImageView)view.findViewById(R.id.list_item_merchant_image);
+            legalNameView = (TextView)view.findViewById(R.id.list_item_merchant_legal_name);
+            //.....
+        }
+    }
+}
---
+++ app/src/main/java/gr/stathis/george/gstathis01151/MerchantFragment.java
@@ -0,0 +1,59 @@
+package gr.stathis.george.gstathis01151;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MerchantFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MerchantFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ *
+ */
+public class MerchantFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    public class MerchantFragment extends Fragment {
+
+        MerchantAdapter merchantAdapter;
+
+        public MerchantFragment() {
+
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            fetchMerchants();
+        }
+
+        private void fetchMerchants(){
+            FetchMerchantsTask fetchMerchantsTask = new FetchMerchantsTask(merchantAdapter);
+            fetchMerchantsTask.execute();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            merchantAdapter = new MerchantAdapter(getActivity(),new ArrayList<Merchant>());
+            ListView merchantListView = (ListView)rootView.findViewById(R.id.listview_merchants);
+            merchantListView.setAdapter(merchantAdapter);
+
+            return rootView;
+        }
+    }
+}
